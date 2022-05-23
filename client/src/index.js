cd3b1b9b80c5bdfc62c519b509e56d84fa19d564
Souptik Sarkar <souptiksarkar4572@gmail.com>
2022-05-23 20:53:40 +0530

    fix: remove react strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -9,9 +9,9 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
+		{/* <React.StrictMode> */}
+		<App />
+		{/* </React.StrictMode> */}
 	</Provider>
 );
 
