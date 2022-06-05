e1782df1446156855ce159e5083813f3051d7776
Souptik Sarkar <souptiksarkar4572@gmail.com>
2022-06-05 10:33:17 +0530

    create fresh new migration after creating new db in hosted server

--- server/prisma/migrations/20220517181116_add_product_type_and_user_to_faq/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "Faq" ADD COLUMN     "productTypeId" INTEGER,
-ADD COLUMN     "userId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Faq" ADD CONSTRAINT "Faq_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Faq" ADD CONSTRAINT "Faq_productTypeId_fkey" FOREIGN KEY ("productTypeId") REFERENCES "ProductType"("id") ON DELETE SET NULL ON UPDATE CASCADE;
