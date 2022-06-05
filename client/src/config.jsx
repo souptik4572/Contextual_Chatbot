19024a222cd5f3d63713a38a1c80f9ef203abe14
Souptik Sarkar <souptiksarkar4572@gmail.com>
2022-06-05 06:56:24 +0530

    feat: initial integration of faqs in chatbot

--- client/src/config.jsx
+++ client/src/config.jsx
@@ -1,135 +1,141 @@
-import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import { Loading } from "react-simple-chatbot";
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Loading } from 'react-simple-chatbot';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllFaqs } from './redux';
 
 export const Answer = (props) => {
-  console.log(props);
-  const [loading, setLoading] = useState(true);
-  const [result, setResult] = useState("");
-  const chosenQuestionId = props.previousStep.value.questionId;
-  const answer = props.previousStep.value.answer;
+	console.log(props);
+	const [loading, setLoading] = useState(true);
+	const [result, setResult] = useState('');
+	const { questionId, answer } = props.previousStep.value;
 
-  const myFunc = async () => {
-    //API CALL
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    // answer to asked question (chosenQuestionId)
-    // NOTE: We can get this in the previous step itself no need to make an extra call
+	const myFunc = async () => {
+		//API CALL
+		// await new Promise((resolve) => setTimeout(resolve, 3000));
+		// answer to asked anFaq (chosenQuestionId)
+		// NOTE: We can get this in the previous step itself no need to make an extra call
 
-    setResult(answer);
-    setLoading(false);
-    // if (no more questions) {
-    //   props.triggerNextStep({trigger: 3})
-    // }
-    props.triggerNextStep({
-      value: chosenQuestionId,
-    });
-  };
+		setResult(answer);
+		setLoading(false);
+		// if (no more questions) {
+		//   props.triggerNextStep({trigger: 3})
+		// }
+		props.triggerNextStep({
+			value: questionId,
+		});
+	};
 
-  useEffect(() => {
-    myFunc();
-  }, []);
+	useEffect(() => {
+		myFunc();
+	}, []);
 
-  return <div>{loading ? <Loading /> : result}</div>;
+	return <div>{loading ? <Loading /> : result}</div>;
 };
 
 export const Question = (props) => {
-  console.log(props);
-  const [loading, setLoading] = useState(true);
-  const [result, setResult] = useState([]);
-  const chosenQuestionId = props.previousStep.value;
+	console.log(props);
+	const [loading, setLoading] = useState(true);
+	const [result, setResult] = useState([]);
+	const chosenQuestionId = props.previousStep.value;
 
-  const myFunc = async () => {
-    //API CALL to get Questions
-    await new Promise((resolve) => setTimeout(resolve, 2500));
+	const { data: filteredFaqs } = useSelector((state) => state.faq);
+	const dispatch = useDispatch();
 
-    // If not the first question
-    if (chosenQuestionId) {
-      //fetch questions where parentFaqId === chosenQuestionId
-    } else {
-      // fetch questions where parentFaqId === -1 & filter(user, kyc, url/page)
-    }
-    setResult([
-      { value: 101, label: "question 1" },
-      { value: 102, label: "question 2" },
-      { value: 103, label: "question 3" },
-      { value: 104, label: "question 4" },
-    ]);
-    setLoading(false);
-  };
+	useEffect(() => {
+		setResult(result.concat(filteredFaqs));
+	}, [filteredFaqs]);
 
-  const renderQuestions = () => {
-    console.log("RESULTS: ", result);
-    return (
-      <List>
-        {result.map((question) => {
-          return (
-            <ListItem
-              alignItems="flex-start"
-              button
-              dense
-              divider
-              disableGutters
-              disablePadding
-              key={question.value}
-            >
-              <ListItemButton
-                onClick={() =>
-                  props.triggerNextStep({
-                    trigger: 2,
-                    value: {
-                      questionId: question.value,
-                      answer: result[question].description,
-                    },
-                  })
-                }
-              >
-                <ListItemText primary={question.label} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
-    );
-  };
+	const myFunc = async () => {
+		//API CALL to get Questions
+		await new Promise((resolve) => setTimeout(resolve, 2500));
 
-  useEffect(() => {
-    myFunc();
-  }, []);
+		// If not the first anFaq
+		if (chosenQuestionId) {
+			//fetch questions where parentFaqId === chosenQuestionId
+			dispatch(getAllFaqs({ parentFaqId: chosenQuestionId }));
+			await new Promise((resolve) => setTimeout(resolve, 2500));
+			if (filteredFaqs.length === 0) dispatch(getAllFaqs({}));
+		} else {
+			// fetch questions where parentFaqId === -1 & filter(user, kyc, url/page)
+			if (filteredFaqs.length === 0) await dispatch(getAllFaqs({}));
+		}
+		setLoading(false);
+	};
 
-  return <div>{loading ? <Loading /> : renderQuestions()}</div>;
+	const renderQuestions = () => {
+		console.log('RESULTS: ', result);
+		return (
+			<List>
+				{result.map((anFaq) => {
+					return (
+						<ListItem
+							alignItems='flex-start'
+							button
+							dense
+							divider
+							disableGutters
+							disablePadding
+							key={anFaq.id}
+						>
+							<ListItemButton
+								onClick={() =>
+									props.triggerNextStep({
+										trigger: 2,
+										value: {
+											questionId: anFaq.id,
+											answer: anFaq.description,
+										},
+									})
+								}
+							>
+								<ListItemText primary={anFaq.question} />
+							</ListItemButton>
+						</ListItem>
+					);
+				})}
+			</List>
+		);
+	};
+
+	useEffect(() => {
+		myFunc();
+	}, []);
+
+	return <div>{loading ? <Loading /> : renderQuestions()}</div>;
 };
 
 // API Reference: https://lucasbassetti.com.br/react-simple-chatbot/#/docs/installation
-export const name = "Rishabh";
+export const name = 'Rishabh';
 export const steps = [
-         {
-           id: "0",
-           message: `Hello ${name}! How can I help you?`,
-           trigger: "1",
-         },
-         {
-           id: "1",
-           waitAction: true,
-           component: <Question />,
-           trigger: "2",
-           asMessage: true,
+	{
+		id: '0',
+		message: `Hello ${name}! How can I help you?`,
+		trigger: '1',
+	},
+	{
+		id: '1',
+		waitAction: true,
+		component: <Question />,
+		trigger: '2',
+		asMessage: true,
 
-           // options: [
-           //   { value: 1, label: "question 1", trigger: "2" },
-           //   { value: 2, label: "question 2", trigger: "2" },
-           //   { value: 3, label: "question 3", trigger: "2" },
-           //   { value: 4, label: "Thank You", trigger: "3" },
-           // ],
-         },
-         {
-           id: "2",
-           waitAction: true,
-           component: <Answer />,
-           trigger: "1",
-         },
-         {
-           id: "3",
-           message: "Bye!",
-           end: true,
-         },
-       ];
+		// options: [
+		//   { value: 1, label: "anFaq 1", trigger: "2" },
+		//   { value: 2, label: "anFaq 2", trigger: "2" },
+		//   { value: 3, label: "anFaq 3", trigger: "2" },
+		//   { value: 4, label: "Thank You", trigger: "3" },
+		// ],
+	},
+	{
+		id: '2',
+		waitAction: true,
+		component: <Answer />,
+		trigger: '1',
+	},
+	{
+		id: '3',
+		message: 'Bye!',
+		end: true,
+	},
+];
