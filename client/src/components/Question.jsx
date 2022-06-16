f24b11e94038c98a3d4ed3f345068ce5202707c6
Souptik Sarkar <souptiksarkar4572@gmail.com>
2022-06-16 23:58:51 +0530

    feat: add dynamic questions for products

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -1,80 +1,88 @@
-import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import { Loading } from "react-simple-chatbot";
-import { useSelector, useDispatch } from "react-redux";
-import { getAllFaqs } from "../redux";
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Loading } from 'react-simple-chatbot';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllFaqs } from '../redux';
 
 const Question = (props) => {
-  const [loading, setLoading] = useState(true);
-  const [result, setResult] = useState([]);
-  const chosenQuestionId = props.previousStep.value;
-  const path = props.path;
+	const [loading, setLoading] = useState(true);
+	const [result, setResult] = useState([]);
+	const chosenQuestionId = props.previousStep.value;
+	const path = props.path;
 
-  const { data: filteredFaqs } = useSelector((state) => state.faq);
-  const dispatch = useDispatch();
+	const [productTypeParam] = path
+		.split('/')
+		.filter((aParam) => aParam !== 'filter')
+		.reverse();
 
-  const myFunc = async () => {
-    // If not the first Faq
-    if (chosenQuestionId) {
-      //fetch questions where parentFaqId === chosenQuestionId
-      let currentFaqs = await dispatch(
-        getAllFaqs({ parentFaqId: chosenQuestionId })
-      );
-      if (currentFaqs.length === 0) {
-        currentFaqs = await dispatch(getAllFaqs({}));
-      }
-      setResult(currentFaqs);
-    } else {
-      // fetch questions where parentFaqId === -1 & filter(user, kyc, url/page using path)
-      console.log(path);
-      console.log(path.split("/"));
-      if (filteredFaqs.length === 0) {
-        let currentFaqs = await dispatch(getAllFaqs({}));
-        setResult(currentFaqs);
-      }
-    }
-    setLoading(false);
-  };
+	const { data: filteredFaqs } = useSelector((state) => state.faq);
+	const { reverseMapper } = useSelector((state) => state.productType);
+	const dispatch = useDispatch();
 
-  const renderQuestions = () => {
-    return (
-      <List>
-        {result.map((anFaq) => {
-          return (
-            <ListItem
-              alignItems="flex-start"
-              button
-              dense
-              divider
-              disableGutters
-              disablePadding
-              key={anFaq.id}
-            >
-              <ListItemButton
-                onClick={() =>
-                  props.triggerNextStep({
-                    trigger: 2,
-                    value: {
-                      questionId: anFaq.id,
-                      answer: anFaq.description,
-                    },
-                  })
-                }
-              >
-                <ListItemText primary={anFaq.question} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
-    );
-  };
+	const myFunc = async () => {
+		// If not the first Faq
+		const filterParams = {};
+		console.log(productTypeParam);
+		if (productTypeParam) filterParams.productTypeId = reverseMapper[productTypeParam];
+		console.log(filterParams);
+		if (chosenQuestionId) {
+			//fetch questions where parentFaqId === chosenQuestionId
+			let currentFaqs = await dispatch(
+				getAllFaqs({ ...filterParams, parentFaqId: chosenQuestionId })
+			);
+			if (currentFaqs.length === 0) {
+				currentFaqs = await dispatch(getAllFaqs({ ...filterParams }));
+			}
+			setResult(currentFaqs);
+		} else {
+			// fetch questions where parentFaqId === -1 & filter(user, kyc, url/page using path)
+			if (filteredFaqs.length === 0) {
+				let currentFaqs = await dispatch(getAllFaqs({ ...filterParams }));
+				setResult(currentFaqs);
+			}
+		}
+		setLoading(false);
+	};
 
-  useEffect(() => {
-    myFunc();
-  }, []);
+	const renderQuestions = () => {
+		return (
+			<List>
+				{result.map((anFaq) => {
+					return (
+						<ListItem
+							alignItems='flex-start'
+							button
+							dense
+							divider
+							disableGutters
+							disablePadding
+							key={anFaq.id}
+						>
+							<ListItemButton
+								onClick={() =>
+									props.triggerNextStep({
+										trigger: 2,
+										value: {
+											questionId: anFaq.id,
+											answer: anFaq.description,
+										},
+									})
+								}
+							>
+								<ListItemText primary={anFaq.question} />
+							</ListItemButton>
+						</ListItem>
+					);
+				})}
+			</List>
+		);
+	};
 
-  return <div>{loading ? <Loading /> : renderQuestions()}</div>;
+	useEffect(() => {
+		myFunc();
+	}, []);
+
+	return <div>{loading ? <Loading /> : renderQuestions()}</div>;
 };
 
 export default Question;
