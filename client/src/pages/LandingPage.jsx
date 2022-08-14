90e880367de75ebc1f1fc02956a9049d2aa92072
Souptik Sarkar <souptiksarkar4572@gmail.com>
2022-08-14 12:23:25 +0530

    feat: add external link to groww.in to prevent copyright

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,53 +1,56 @@
-import * as React from "react";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-import Container from "@mui/material/Container";
-import { ArrowForward } from "@mui/icons-material";
-import TabBar from "../components/TabBar";
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Container from '@mui/material/Container';
+import { ArrowForward } from '@mui/icons-material';
+import TabBar from '../components/TabBar';
 
 const LandingPage = () => {
-  return (
-    <React.Fragment>
-      <Container
-        disableGutters
-        maxWidth="lg"
-        component="main"
-        sx={{ pt: 8, pb: 6 }}
-      >
-        <Typography
-          component="h1"
-          variant="h2"
-          align="center"
-          color="text.primary"
-          gutterBottom
-        >
-          GROWW
-        </Typography>
-        <Typography
-          variant="h5"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Trusted by millions of Indians. Start investing today.
-        </Typography>
-      </Container>
-      {/* TabBar */}
-      <TabBar />
-      {/* Products Page Link */}
-      <Typography
-        variant="h5"
-        align="center"
-        sx={{
-          mt: 2,
-        }}
-      >
-        <Link href="/products" underline="hover">
-          View all Products <ArrowForward size="small" />
-        </Link>
-      </Typography>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Container disableGutters maxWidth='lg' component='main' sx={{ pt: 8, pb: 6 }}>
+				<Typography
+					component='h1'
+					variant='h2'
+					align='center'
+					color='text.primary'
+					gutterBottom
+				>
+					GROWW{' '}
+					<Typography
+						component='h6'
+						variant='h6'
+						align='center'
+						color='text.primary'
+						gutterBottom
+					>
+						(Inspired from{' '}
+						<Link href='https://groww.in/' target='_blank'>
+							groww.in
+						</Link>
+						)
+					</Typography>
+				</Typography>
+				<Typography variant='h5' align='center' color='text.secondary' component='p'>
+					Trusted by millions of Indians. Start investing today.
+				</Typography>
+			</Container>
+			{/* TabBar */}
+			<TabBar />
+			{/* Products Page Link */}
+			<Typography
+				variant='h5'
+				align='center'
+				sx={{
+					mt: 2,
+				}}
+			>
+				<Link href='/products' underline='hover'>
+					View all Products <ArrowForward size='small' />
+				</Link>
+			</Typography>
+		</React.Fragment>
+	);
 };
 
 export default LandingPage;
